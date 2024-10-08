30913cf29124df4e6eaf8e347cec5e4e60070213
aroussel-data <alexandredrr14@gmail.com>
2024-10-08 23:00:20 +0200

    added extra nvim plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,13 @@
 require("config.lazy")
 
+vim.cmd.colorscheme "catppuccin"
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
